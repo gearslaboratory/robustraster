91e2d4b136e79747f92aeabbefc2979611cedc33
adrianom-gh <adrianom@unr.edu>
2024-09-08 20:12:19 -0700

    draft udf tuner.

--- src/udf_tuner.py
+++ src/udf_tuner.py
@@ -1,10 +1,43 @@
 import xarray as xr
 import dask.array as da
+import dask
+import psutil
+import time
 
 class UserDefinedFunction:
     def __init__(self):
         pass
 
+    def _tune_user_function(self, ds, user_func, *args, **kwargs):
+        # Dynamically determine dimension names
+        dim_names = list(ds.dims.keys())
+        
+        # Extract a single chunk to determine the output structure using dynamic dimension names
+        one_chunk_slices = {dim: slice(0, ds.chunks[dim][0]) for dim in dim_names}
+        one_chunk = ds.isel(**one_chunk_slices)
+        
+        test = xr.map_blocks(self._user_function_wrapper, 
+                               one_chunk, 
+                               args=(user_func,) + args, 
+                               kwargs=kwargs)
+        start_time = time.time()
+        test.compute()
+        end_time = time.time()
+        
+        wall_time = end_time - start_time
+        memory_usage = self._log_memory_usage()
+        print(f"Wall time: {wall_time:.2f} seconds")
+        print(f"Memory usage: {memory_usage:.2f} MB")
+
+        # Apply the processing function to this chunk
+        #processed_chunk = user_func(one_chunk)
+
+        return test
+    
+    def _log_memory_usage(self):
+        process = psutil.Process()
+        return process.memory_info().rss / 1024**2  # Memory usage in MB
+        
     def _generate_template_xarray(self, ds, user_func):
         # Dynamically determine dimension names
         dim_names = list(ds.dims.keys())
@@ -42,7 +75,7 @@ class UserDefinedFunction:
         )
         
         return template
-
+    
     def _user_function_wrapper(self, ds, user_func, *args, **kwargs):
         """
         Apply a user-defined function to the Dask DataFrame.
@@ -81,8 +114,15 @@ class UserDefinedFunction:
                                kwargs=kwargs, 
                                template=template)
         '''
+        # Run tests here! Then jump to the real run! #
+        test = self._tune_user_function(ds, user_func, *args, **kwargs)
+        '''
+        # If I load the dataset as a dask delayed, I don't need a template!
         result = xr.map_blocks(self._user_function_wrapper, 
                                ds, 
                                args=(user_func,) + args, 
                                kwargs=kwargs)
+
         return result
+        '''
+        return test
