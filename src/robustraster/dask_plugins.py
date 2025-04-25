fa366a3b120aef3d59bea33b6c23562f29cfdd0c
adrianom-gh <adrianom@unr.edu>
2025-04-24 19:03:42 -0700

    Per Jonathan's request, Earth Engine will now be authenticate via ee.Authenticate() and not via a service account credentials JSON file. Modified my Dask EE plugin with this change. Modified my requirements.txt file to include missing packages: geopandas, gcsfs.

--- src/robustraster/dask_plugins.py
+++ src/robustraster/dask_plugins.py
@@ -2,7 +2,7 @@ import os
 import json
 import ee
 from distributed import WorkerPlugin
-
+"""
 class EEPlugin(WorkerPlugin):
     def __init__(self, json_key: str = None):
         self.json_key = json_key
@@ -30,3 +30,16 @@ class EEPlugin(WorkerPlugin):
 
     def release_key(self, key, state, cause, reason, report):
         pass
+"""
+class EEPlugin(WorkerPlugin):
+    def __init__(self, json_key: str = None):
+        self.json_key = json_key
+
+    def setup(self, worker):
+        self.worker = worker
+        try:
+            # Assume credentials already exist at default location
+            ee.Initialize(opt_url='https://earthengine-highvolume.googleapis.com')
+        except ee.EEException as e:
+            raise RuntimeError("Earth Engine initialization failed. "
+                            "Run ee.Authenticate() once on the client before starting the cluster.")
